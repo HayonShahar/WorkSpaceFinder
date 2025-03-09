e47676e3b91974643de08aad32f92b1ee3d579b3
AntonHorelyk <anton.cool712@gmail.com>
2025-03-09 18:33:34 +0200

    please

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -9,9 +9,9 @@ import ResultsPage from './pages/ResultsPage';
 import WorkplacePage from './pages/WorkplacePage'; 
 import RegisterContainer from './components/register/RegisterContainer';
 import LoginContainer from './components/login/LoginContainer';
+import Contact from './pages/Contact';
 import './App.css'; 
 import TFAContainer from './components/TFA/TFAContainer';
-import Promote from './pages/Promote';
 
 const App = () => {
   const location = useLocation();
@@ -29,10 +29,10 @@ const App = () => {
         <Route path="/addworkplace" element={<AddWorkplace />} /> 
         <Route path="/results" element={<ResultsPage />} />
         <Route path="/workplace/:id" element={<WorkplacePage />} />
+        <Route path="/Contact" element={<Contact />} />
         <Route path="/register" element={<RegisterContainer />} />
         <Route path="/login" element={<LoginContainer />} />
         <Route path="/tfa" element={<TFAContainer />} />
-        <Route path="/promote" element={<Promote />} />
       </Routes>
 
       {/* Render Footer only if the current path is NOT '/register', '/login', or '/tfa' */}
