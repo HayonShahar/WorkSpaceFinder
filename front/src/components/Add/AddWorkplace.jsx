7a9329ed1ade4ea52d34f3d53604864c00ec3756
Shahar Hayon <shaharhayon12300@gmail.com>
2025-03-10 13:55:30 +0200

    fixed register and add space

--- front/src/components/Add/AddWorkplace.jsx
+++ front/src/components/Add/AddWorkplace.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 
 import '../../styles/AddWorkplace.css';
 
@@ -10,10 +10,16 @@ const AddWorkplace = () => {
     const [file, setFile] = useState('');
     const [description, setDescription] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const userId = localStorage.getItem("userId");
 
     const handleSubmit = async (e) => {
         if (!file) {
             setErrorMessage("Something went wrong with photo upload");
+            setTimeout(() => {
+                setErrorMessage('');
+            }, 2000); // Hide error message after 2 seconds
             return;
         }
 
@@ -23,7 +29,7 @@ const AddWorkplace = () => {
         const UPLOAD_PRESET = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET;
         console.log(UPLOAD_PRESET);
 
-        formData.append('file', file); 
+        formData.append('file', file);
         formData.append('folder', 'workspaces');
         formData.append('upload_preset', UPLOAD_PRESET);
 
@@ -39,8 +45,10 @@ const AddWorkplace = () => {
                 address,
                 type,
                 description,
-                image_url: uploadedImageUrl
+                image_url: uploadedImageUrl,
+                upload_user_id: userId
             };
+            console.log('Workplace:', workplaceData);
 
             const serverResponse = await axios.post('http://localhost:8080/api/workSpace', workplaceData);
             console.log('Workplace added successfully:', serverResponse.data);
@@ -51,9 +59,17 @@ const AddWorkplace = () => {
             setDescription('');
             setFile('');
             setErrorMessage('');
+            setSuccessMessage(serverResponse.data.message);
+
+            setTimeout(() => {
+                setSuccessMessage('');
+            }, 2000); // Hide success message after 2 seconds
         } catch (error) {
             console.error('Error uploading image or saving workplace:', error);
             setErrorMessage("There was an error uploading or saving the workplace.");
+            setTimeout(() => {
+                setErrorMessage('');
+            }, 2000); // Hide error message after 2 seconds
         }
     };
 
@@ -123,7 +139,10 @@ const AddWorkplace = () => {
                 </div>
 
                 <button type="submit">Add Workplace</button>
-                <span className='errorMessage'>{errorMessage}</span>
+                <div className='add-workspace-message'>
+                    <span className='errorMessage'>{errorMessage}</span>
+                    <span className='successMessage'>{successMessage}</span>
+                </div>
             </form>
         </div>
     );
